af3b9cec1ff3d16dcdd935ca2af900a7ea12ec24
Iverson Andales <kintol867@gmail.com>
2026-05-21 13:42:14 +0800

    fix db.ts to use environment variables

--- node-mysql-api/_helpers/db.ts
+++ node-mysql-api/_helpers/db.ts
@@ -9,19 +9,16 @@ const db: any = {};
 initialize();
 
 async function initialize() {
-  const { host, port, user, password, database } = config.database;
-  const connection = await mysql.createConnection({
-    host,
-    port,
-    user,
-    password,
-  });
-
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+  const host     = process.env.DB_HOST     || config.database.host;
+  const port     = parseInt(process.env.DB_PORT || String(config.database.port));
+  const user     = process.env.DB_USER     || config.database.user;
+  const password = process.env.DB_PASSWORD || config.database.password;
+  const database = process.env.DB_NAME     || config.database.database;
 
   const sequelize = new Sequelize(database, user, password, {
     dialect: "mysql",
     host: host,
+    port: port,
   });
 
   db.Account = accountModel(sequelize);
