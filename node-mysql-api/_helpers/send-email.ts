2f49a0bf31e2691d521e790816cb1dc9b9e04dc8
Iverson Andales <kintol867@gmail.com>
2026-05-21 18:12:17 +0800

    add email debug logging

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -8,5 +8,6 @@ export default async function sendEmail({
   from = config.emailFrom,
 }: any) {
   const transporter = nodemailer.createTransport(config.smtpOptions as any);
-  await transporter.sendMail({ from, to, subject, html });
+  const info = await transporter.sendMail({ from, to, subject, html });
+  console.log("Email sent to:", to, "Message ID:", info.messageId, "Preview:", nodemailer.getTestMessageUrl(info));
 }
