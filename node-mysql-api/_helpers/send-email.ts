354f1a9c6fadbfc7b2a72d7e8e676367942518f7
Iverson Andales <kintol867@gmail.com>
2026-05-21 18:55:45 +0800

    done

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -1,21 +1,26 @@
 import nodemailer from "nodemailer";
-import config from "../config.json";
 
 export default async function sendEmail({
   to,
   subject,
   html,
-  from = config.emailFrom,
 }: any) {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
     } as any);
-    const info = await transporter.sendMail({ from: process.env.SMTP_USER, to, subject, html });
+    const info = await transporter.sendMail({ 
+      from: process.env.SMTP_USER, 
+      to, 
+      subject, 
+      html 
+    });
     console.log("Email sent to:", to, "Message ID:", info.messageId);
   } catch (err: any) {
     console.error("EMAIL ERROR:", err.message);
